43b3f075200089fa5a49918dc5596f4f35d7f05e
elkingrana2 <elkingc1@gmail.com>
2022-10-20 23:00:35 -0500

    email

--- app/schema.sql
+++ app/schema.sql
@@ -52,4 +52,4 @@ CREATE TABLE credentials(
   password TEXT NOT NULL
 );
 
-INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','eagranados@uninorte.edu.co', 'Grana2_99');
+INSERT INTO credentials (name,user,password) VALUES ('EMAIL_APP','developmentcapstone', 'TR41NC0URS3R4');
